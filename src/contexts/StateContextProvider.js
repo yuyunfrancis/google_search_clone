d0acfc57cc80ada781779cb9420893fbee6e5f22
yuyunfrancis <yuyun.francis@ictuniversity.org>
2022-01-14 19:18:58 +0100

    google clone

---
+++ src/contexts/StateContextProvider.js
@@ -0,0 +1,37 @@
+import React, { createContext, useContext, useState } from "react";
+
+const StateContext = createContext();
+const baseUrl = "https://google-search3.p.rapidapi.com/api/v1";
+
+export const StateContextProvider = ({ children }) => {
+  const [results, setResults] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [searchTerm, setSearchTerm] = useState("");
+
+  const getResults = async (url) => {
+    setLoading(true);
+
+    const res = await fetch(`${baseUrl}${url}`, {
+      method: "GET",
+      headers: {
+        "x-rapidapi-host": "google-search3.p.rapidapi.com",
+        "x-rapidapi-key": "1de5f4b316msh0fa0c2e97e146e8p103917jsn17a8557871a1",
+      },
+    });
+
+    const data = await res.json();
+
+    setResults(data);
+    setLoading(false);
+  };
+
+  return (
+    <StateContext.Provider
+      value={{ getResults, results, searchTerm, setSearchTerm, loading }}
+    >
+      {children}
+    </StateContext.Provider>
+  );
+};
+
+export const useStateContext = () => useContext(StateContext);
